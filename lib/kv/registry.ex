d62a37e7b5f6d9c5bb377f8b38ca8e89849b984a
Matheus Raposo <matheus_raposo@hotmail.com>
2022-07-16 10:53:19 -0300

    add documentation for client api

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -7,12 +7,18 @@ defmodule KV.Registry do
     GenServer.start_link(__MODULE__, :ok, opts)
   end
 
+  @doc """
+  Looks up the bucket pid for `name` stored in `server`.
+
+  Returns `{:ok, pid}` if the bucket exists, `:error` otherwise.
+  """
   def lookup(server, name) do
     GenServer.call(server, {:lookup, name})
   end
 
   @doc """
-    Would normally be implemented as a synchronous call.
+    Ensures there is a bucket associated with the given `name` in `server`.
+    This would normally be implemented as a synchronous call.
     Asynchronous here only for illustration purposes.
   """
   def create(server, name) do
