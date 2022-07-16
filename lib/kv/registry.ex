b0579b67629b6220a1cb77a78cb68b8b1ee3b51a
Matheus Raposo <matheus_raposo@hotmail.com>
2022-07-16 10:47:41 -0300

    add genserver client api

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -1,6 +1,26 @@
 defmodule KV.Registry do
   use GenServer
 
+  ## Client API
+
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  def lookup(server, name) do
+    GenServer.call(server, {:lookup, name})
+  end
+
+  @doc """
+    Would normally be implemented as a synchronous call.
+    Asynchronous here only for illustration purposes.
+  """
+  def create(server, name) do
+    GenServer.cast(server, {:create, name})
+  end
+
+  ## GenServer Callbacks
+
   @impl true
   def init(:ok) do
     {:ok, %{}}
