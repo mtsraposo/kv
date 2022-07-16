d568860261a29a988ca61f2a37a76774c164fea8
Matheus Raposo <matheus_raposo@hotmail.com>
2022-07-16 10:02:52 -0300

    add delete function

--- lib/kv/bucket.ex
+++ lib/kv/bucket.ex
@@ -12,4 +12,8 @@ defmodule KV.Bucket do
   def put(agent, key, value) do
     Agent.update(agent, &Map.put(&1, key, value))
   end
+
+  def delete(agent, key) do
+    Agent.get_and_update(agent, &Map.pop(&1, key))
+  end
 end
