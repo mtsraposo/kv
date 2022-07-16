1f3955af7e786a944729724945c8d1e507097f38
Matheus Raposo <matheus_raposo@hotmail.com>
2022-07-16 16:32:17 -0300

    add dynamic supervisor for buckets

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -22,4 +22,12 @@ defmodule KV.RegistryTest do
     Agent.stop(bucket)
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
+
+  test "removes bucket on crash", %{registry: registry} do
+    KV.Registry.create(KV.Registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(KV.Registry, "shopping")
+
+    Agent.stop(bucket, :shutdown)
+    assert KV.Registry.lookup(KV.Registry, "shopping") == :error
+  end
 end
